82e01b93b38f1d5002b3f2041ab17d25360170c3
zachary mines <zmines@wesleyan.edu>
2020-08-19 04:53:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(books.title) FROM characters, books, character_books WHERE character_books.character_id = characters.id AND character_books.book_id  = books.id ORDER BY COUNT(books.title) DESC;"
+  "SELECT characters.name, COUNT(books.title) FROM characters, books, character_books WHERE character_books.character_id = characters.id AND character_books.book_id  = books.id GROUP BY characters.name ORDER BY COUNT(books.title) DESC;"
 end 
